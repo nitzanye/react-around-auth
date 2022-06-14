00d2638394033cb5873dcab38b281988f9a91870
NitzanSmulevici <nitzanye@gmail.com>
2022-06-15 01:15:57 +0300

    add btn

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React from 'react';
 import successImage from '../images/successImage.svg';
 import failImage from '../images/failImage.svg';
 
 const InfoTooltip = ({ isOpen, onClose, isSuccess }) => {
-    
-    return (
-        <div className={`popup popup_type_toolip ${isOpen && 'popup_opened'}`} >
-          <div className="popup__window popup__window_type_tooltip">
-            <img src={isSuccess ? successImage : failImage} alt="Tooltip image" className="popup__tooltip-image" />
-            <p className="popup__message">
-                {isSuccess ? `Success! You have now been registered.` : `Oops, something went wrong! Please try again.`}
-            </p>
-            <button
-              className={`button_type_close button_type_close-tooltip`}
-              type="button"
-              onClick={onClose}
-            ></button>
-          </div>
-        </div>
-      );
-}
+  return (
+    <div className={`popup popup_type_toolip ${isOpen && 'popup_opened'}`}>
+      <div className='popup__window popup__window_type_tooltip'>
+        <img
+          src={isSuccess ? successImage : failImage}
+          alt='Tooltip image'
+          className='popup__tooltip-image'
+        />
+        <p className='popup__message'>
+          {isSuccess
+            ? `Success! You have now been registered.`
+            : `Oops, something went wrong! Please try again.`}
+        </p>
+        <button
+          className='button_type_close-tooltip'
+          type='button'
+          onClick={onClose}
+        ></button>
+      </div>
+    </div>
+  );
+};
 
 export default InfoTooltip;
-
-

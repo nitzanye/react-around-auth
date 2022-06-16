5255268681c13ea5542877307f31cb53003628f0
NitzanSmulevici <nitzanye@gmail.com>
2022-06-16 04:11:23 +0300

    add hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,79 +1,111 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.svg';
-import hamburgerOpen from '../images/hamburgerOpen.svg';
-import hamburgerClose from '../images/hamburgerClose.svg';
 
 const Header = (props) => {
   const { loggedIn, userEmail, handleLogout, linkPath, linkText } = props;
 
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
   const [isDropDownOpen, setisDropDownOpen] = React.useState(false);
 
-  const toggleHamburger = () => {
-    setisDropDownOpen(!isDropDownOpen);
-  };
+  const toggleHamburger = () => setisDropDownOpen(!isDropDownOpen);
+  const handleNavClick = () => setisDropDownOpen(false);
 
-  const handleLogOutFromHamburger = () => {
-    toggleHamburger();
-    handleLogout();
+  React.useEffect(() => {
+    setisDropDownOpen(false);
+  }, [setisDropDownOpen]);
+
+  React.useEffect(() => {
+    let timeoutId = null;
+    const handleResize = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setWindowWidth(window.innerWidth);
+      }, 150);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+
+  const setHeader = () => {
+    if (!loggedIn) {
+      return (
+        <nav className='header__menu'>
+          <Link className='header__link' to={linkPath}>
+            {linkText}
+          </Link>
+        </nav>
+      );
+    }
+
+    if (loggedIn && windowWidth > 600) {
+      return (
+        <nav className='header__menu'>
+          <ul className='header__menu-container'>
+            <li>
+              <p className='header__email'>{userEmail}</p>
+            </li>
+            <li>
+              <button className='header__link' onClick={handleLogout}>
+                {linkText}
+              </button>
+            </li>
+          </ul>
+        </nav>
+      );
+    }
+    return (
+      <div className='header__hamburger'>
+        <button
+          className={`${
+            isDropDownOpen
+              ? `header__hamburger-icon header__hamburger-close`
+              : `header__hamburger-icon`
+          }`}
+          type='button'
+          onClick={toggleHamburger}
+        ></button>
+        <nav
+          className={`${
+            isDropDownOpen
+              ? `header__hambuger-menu header__hamburger-menu_visible`
+              : `header__hambuger-menu`
+          }`}
+          onClick={handleNavClick}
+        >
+          <ul className='header__hamburger-menu-container'>
+            <li className='header__hamburger-menu-item'>
+              <p className='header__hamburger-email'>{userEmail}</p>
+            </li>
+            <li className='header__hamburger-menu-item'>
+              <button className='header__hamburger-link' onClick={handleLogout}>
+                {linkText}
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
   };
 
   return (
-    <>
-      {loggedIn && (
-        <div
-          className={
-            isDropDownOpen ? 'header__menu header__menu-open' : 'header__menu'
-          }
-        >
-          <p className='header__hamburger-email'>{userEmail}</p>
-          <Link
-            to={linkPath}
-            onClick={handleLogOutFromHamburger}
-            className='header__hamburger-link'
-          >
-            {linkText}
-          </Link>
-        </div>
-      )}
-      <header className='header'>
-        <Link to='/'>
-          <img
-            className='logo logo__image'
-            src={logo}
-            id='logo'
-            alt='Around the U.S.'
-          />
-        </Link>
-        {loggedIn ? (
-          <div>
-            <nav className='header__menu-container'>
-              <p className='header__email'>{userEmail}</p>
-              <Link
-                to={linkPath}
-                onClick={handleLogout}
-                className='header__link'
-              >
-                {linkText}
-              </Link>
-            </nav>
-            <button className='header__hamburger' onClick={toggleHamburger}>
-              <img
-                src={!isDropDownOpen ? hamburgerOpen : hamburgerClose}
-                className='header__hamburger-icon'
-                alt=''
-              />
-            </button>
-          </div>
-        ) : (
-          <nav className='header__menu-container'>
-            <Link to={linkPath} className='header__link'>
-              {linkText}
-            </Link>
-          </nav>
-        )}
-      </header>
-    </>
+    <header
+      className={`${
+        isDropDownOpen && windowWidth <= 600
+          ? `header header__menu header__menu-open`
+          : ` header header__menu`
+      }`}
+    >
+      <img
+        className='logo logo__image'
+        src={logo}
+        id='logo'
+        alt='Around the U.S.'
+      />
+      <div className='header__content'>{setHeader()}</div>
+    </header>
   );
 };
 

d25968bd6db4e47ae5334df4ca1b10b67cdcad6a
NitzanSmulevici <nitzanye@gmail.com>
2022-07-11 22:50:21 +0300

    fix api

--- src/components/App.js
+++ src/components/App.js
@@ -84,7 +84,7 @@ const App = () => {
         })
         .catch((err) => console.log(`Error.....: ${err}`));
     }
-  }, [loggedIn]);
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
